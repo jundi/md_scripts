51ca560d8feffe8b238c031be9cadbc68b17011d
Heikki Mikkolainen <mikkolah@fys104.phys.tut.fi>
2014-07-30 18:21:15 +0300

    diff

--- free_energy.py
+++ free_energy.py
@@ -4,58 +4,72 @@ import numpy
 import argparse
 import matplotlib.pyplot as plt
 import scipy.integrate
+import progressbar as pb
 
 
 # parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", help="list of windows and pullf.xvg -files")
 parser.add_argument("-b", help="first time step to be used")
+parser.add_argument("-dt", help="use frame only when t MOD dt = first time (ps)")
 args = parser.parse_args()
 if args.f:
     files = args.f
 else:
-    exit()
+    if __name__ == "__main__":
+        exit()
 if args.b:
     first_tstep = float(args.b)
 else:
     first_tstep = 0
+if args.dt:
+    dt = float(args.dt)
+else:
+    dt = 1
 
 
 
-# Reads one pullf.xvg -file.
-def read_xvg(pullf_file_name):
+def read_xvg(pullf_file_name, dt):
+    '''Reads one pullf.xvg -file.'''
     
     # Open file
     pullf_file=open(pullf_file_name, 'r')
     # Init array
     forces=[]
     
+    # ps to fs
+    dt = round(1000*float(dt))
+
     # Read file
     for line in pullf_file:
         # Skip comment/header lines 
         if (line.startswith('#') or  line.startswith('@')):
             continue
 
-	# Add force to array
         timestep=float(line.split()[0])
+
+	# check the timestep
+        if round(1000*timestep) % dt != 0:
+            continue
+
+        # Add force to array
         force=float(line.split()[1])
         forces.append([timestep,force])
     
     # Close file
     pullf_file.close()
      
-    # create numpy-array
-    forces_np = numpy.array(forces)
-
     # Return mean force
-    return forces_np
+    return forces
 
 
 
 
-# Reads list of z-coordinates and corresponding pullf.xvg -files.
-# Returns z-zoordinates and mean forces.
-def get_force_data(list_file_name):
+
+def get_force_data(list_file_name, dt):
+    '''Reads list of z-coordinates and corresponding pullf.xvg -files.  Returns
+    list of z-zoordinates and list of numpy-arrays of pull-forces as function
+    of time.'''
 
     z=[] # init z-coord vector
     tf_list=[] # list of time-force functions
@@ -64,8 +78,9 @@ def get_force_data(list_file_name):
     list_file=open(list_file_name, 'r')
     for line in list_file:
         pullf_file_name=line.split()[1]
-	pullf_data=read_xvg(pullf_file_name)
-        tf_list.append(pullf_data)
+        print('reading ' + pullf_file_name + '\r', end='')
+        pullf_data = read_xvg(pullf_file_name, dt)
+        tf_list.append(numpy.array(pullf_data))
         z.append(float(line.split()[0]))
     list_file.close()
 
@@ -73,27 +88,98 @@ def get_force_data(list_file_name):
     z = numpy.multiply(z[::-1], -1)
     tf_list = tf_list[::-1]
     for tf in tf_list:
-	tf[:,1] = numpy.multiply(tf[:,1], -1)
+        tf[:,1] = numpy.multiply(tf[:,1], -1)
 
     return z, tf_list
     
 
-    
-# calculate deltaG 
-def calc_deltaG(z,tf_list,first_tstep):
-    
+
+def skip_frames(tf_list, first_tstep):
+    '''Skips frames before 'first_tstep'.'''
+
+    tf_list_equil=[]
+    for tf in tf_list:
+        # skip non-equiliberated time steps
+        tf_list_equil.append(tf[ numpy.where( tf[:,0] >= first_tstep ) ])
+
+    return tf_list_equil
+
+
+
+def calc_mean_forces(tf_list):
+    '''Calculates mean force'''
+
     f_mean=[]
     for tf in tf_list:
-	# skip non-equiliberated time steps
-	tf_equil = tf[ numpy.where( tf[:,0] >= first_tstep ) ]
-	# calculate mean force
-	f_mean.append(tf_equil[:,1].mean())
+        # calculate mean force
+        f_mean.append(tf[:,1].mean())
+
+    return numpy.array(f_mean)
+
+
     
+def calc_deltaG(z, tf_list, first_tstep):
+    '''Calculates deltaG'''
+    
+    # skip frames before 'first_tstep'
+    tf_list=skip_frames(tf_list, first_tstep)
+
+    # get mean force -vector
+    f_mean = calc_mean_forces(tf_list)
+
     # integrate f_mean as function of z
-    deltaG=scipy.integrate.cumtrapz(f_mean,z,initial=0)
+    deltaG = scipy.integrate.cumtrapz(f_mean,z,initial=0)
 
     return deltaG, f_mean
 
+
+
+
+def autocor(x):
+    '''Calculates autocorrelation function of x'''
+    result = numpy.correlate(x, x, mode='full') / len(x)
+    return result[result.size/2:]
+
+
+
+
+def calc_diffcoef(z_list,tf_list,first_tstep):
+    '''Calculates diffusion coefficient D'''
+    
+    # skip frames before 'first_tstep'
+    tf_list=skip_frames(tf_list, first_tstep)
+
+    # get mean force -vector
+    f_mean_list = calc_mean_forces(tf_list)
+
+    # deltaF 
+    deltaF_list = []
+    for tf,f_mean in zip(tf_list, f_mean_list):
+        deltaF_list.append( tf[:,1] - f_mean )
+
+    # time 
+    time_list = []
+    for tf in tf_list:
+        time_list.append(tf[:,0])
+    # shifted_time 
+    #shifted_time = numpy.add(tf_list[:,:,0].transpose(), -tf_list[:,0,0]).transpose()
+
+    # Autocorrelation function <deltaF(z,t),deltaF(z,0)>
+    acor_list=[]
+    for deltaF,z in zip(deltaF_list,z_list):
+        print('calculating autocorrelation functions ' + str(z) + '\r', end='' )
+        acor_list.append( autocor(deltaF) )
+
+
+
+    # Diffusion coefficient FIXME
+    D_list = []
+    for acor,time,z in zip(acor_list,time_list,z_list):
+        print('calculating diffusion coefficient ' + str(z) + '\r', end='' )
+        D  = numpy.trapz(acor,time)
+        D_list.append(D)
+    
+    return D_list
 
 
 
@@ -101,12 +187,33 @@ def calc_deltaG(z,tf_list,first_tstep):
 if __name__ == "__main__":
 
     # get data
-    z, tf_list = get_force_data(files)
-    
+    z, tf_list = get_force_data(files,dt)
+
+    # get data from npy
+    #z, tf_list = get_force_data_npy(files)
+
+    # save data for reuse
+    #numpy.save(files+'.npy',tf_list)
+
     # calculate deltaG
-    deltaG, f_mean = calc_deltaG(z,tf_list,first_tstep)
+    deltaG, f_mean = calc_deltaG(z, tf_list, first_tstep)
+
+    # save deltaG
+    #dG_plot = numpy.array([z, deltaG])
+    #numpy.savetxt('deltaG', dG_plot.transpose())
     
-    # plot
+    # plot dG
     plt.plot(z, f_mean, 'ro', z, deltaG, 'b-')
     plt.show()
 
+    # calculate diffusion coefficient
+    D=calc_diffcoef(z, tf_list, first_tstep)
+    #print(D)
+
+    # plot diffcoeff.
+    plt.plot(z, D)
+    plt.show()
+
+    #numpy.savetxt('tf_list', tf_list[1])
+    #numpy.savetxt('D', D[1])
+    #numpy.savetxt('f_mean', f_mean)
