5ffb9dd83a5bb36ff8e68db329a7563a140f8bc3
h. mikkolainen <heikki.mikkolainen@gmail.com>
2014-10-09 13:23:10 +0300

    argparse

--- python/agr_cmnts2lgnds.py
+++ python/agr_cmnts2lgnds.py
@@ -2,24 +2,42 @@
 '''
 This script names data sets of .agr-file according to comments.
 '''
-import sys
+#import sys
 import re
+import argparse
 
-infile_name=sys.argv[1]
-outfile_name=sys.argv[2]
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', default=-1, help='which word to use')
+parser.add_argument('-f', help='input')
+parser.add_argument('-o', help='output')
+args = parser.parse_args()
+
+if args.f:
+    infile_name=args.f
+else:
+    print('input file missing')
+    exit()
+if args.o:
+    outfile_name=args.o
+else:
+    print('output file missing')
+    exit()
+
+
 
 def choose_legend(comment,n):
     words=re.split('\.|/|"',comment)
     nonempty_words=[x for x in words if x]
     if n == -1:
         print(nonempty_words)
-        n=int(input('Which? (0-' + str(len(nonempty_words)) + ')'))
+        n=int(input('Which? (0-' + str(len(nonempty_words)-1) + ')'))
     return (nonempty_words[n],n)
 
 
 
 newfile=[]
-comment_num=-1
+comment_num=int(args.c)
 with open(infile_name,'r') as infile:
     for line in infile:
         words=line.split()
@@ -28,7 +46,7 @@ with open(infile_name,'r') as infile:
         if len(words) > 2 and words[2] == 'legend':
             legend, comment_num = choose_legend(comment,comment_num)
             print(legend)
-            newfile.append(' '.join(words[0:3]) + legend + '\n')
+            newfile.append(' '.join(words[0:3]) + ' "' + legend+ '"' + '\n')
         else:
             newfile.append(line)
 
