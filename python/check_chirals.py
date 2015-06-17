11f5a590263606f2f2fc4173240630a0d2162196
h. mikkolainen <heikki.mikkolainen@gmail.com>
2015-06-17 13:48:22 +0300

    output formatting

--- python/check_chirals.py
+++ python/check_chirals.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python2
-
+"""
+Checks the R/S-handedness of molecules stereocenters based on the coordinates of
+the four atoms bound to a chiral center.
+"""
 import argparse 
 import MDAnalysis
 import numpy 
 import sys
 
 
+
 ### DEFAULTS
 coordinatefile="confout.gro"
 resname="LBPA"
-atomnames=["O21'", "C1'", "C3'", "HS'", "O21", "C1", "C3", "HS"]
+atomnames=["O21", "C1", "C3", "HS", "O21'", "C1'", "C3'", "HS'"]
 
 
 
 ### PARSE ARGUMENTS
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument("-c", help='Coordinate file')
 parser.add_argument("-a", help='Atoms bound to the chiral center (in order of'
                                ' priority). If there is n chiral centers, 4n '
@@ -24,13 +28,19 @@ args = parser.parse_args()
 
 if args.c:
     coordinatefile = args.c
+else:
+    print("Using default coordinate file: " + coordinatefile)
 if args.a:
     if len(args.a) % 4 > 0:
         sys.exit("Wrong number of atoms.")
     else:
         atomnames = args.a
+else:
+    print("Using default atoms: " + resname)
 if args.r:
     resname = args.r
+else:
+    print("Using default resname: " + ", ".join(atomnames))
 
 
 
@@ -51,7 +61,7 @@ for r in resids:
 
         # get coordinates of four atoms
         coord = []
-        for n in atomnames[c_itr:c_itr+4]:
+        for n in atomnames[4*c_itr:4*c_itr+4]:
             atom = universe.selectAtoms("resid " + str(r) + " and name " + n)
             coord.append(atom.positions[0])
 
@@ -62,13 +72,15 @@ for r in resids:
 
         # which side of the plane is the fourth atom?
         com123 = (coord[0] + coord[1] + coord[2])/3 # center of mass
-        vec4 = com123 - coord[3]                    # vector from the plane to atom4
+        vec4 = coord[3] - com123                    # vector from the plane to atom4
 
         # Is the the plane normal vector parallel or antiparallel with vec4?
         dotp = numpy.dot(crs123, vec4)              # dot product
         if dotp < 0:
+            # angle bigger than 90 degrees -> antiparallel -> "Sinister"
             handedness.append('S')
         else:
+            # angle smaller than 90 degrees -> parallel -> "Rectus"
             handedness.append('R')
 
-    print(resname + str(r) + ": " + " ".join(handedness))
+    print("{0}{1:5}{2}".format(resname, str(r), " ".join(handedness)))
