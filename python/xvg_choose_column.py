9bdaa900b791aee7372f0fc9c823e37119841ff7
h. mikkolainen <heikki.mikkolainen@gmail.com>
2015-07-01 14:24:06 +0300

    argparse

--- python/xvg_choose_column.py
+++ python/xvg_choose_column.py
@@ -1,28 +1,40 @@
 #!/usr/bin/env python
 #
 # Rips one column from multicolumn xvg to new file. New file is named as
-# "oldfile-column_n.xvg"
+# "oldfile-column_n.xvg" if output file is not given.
 #
 # Example usage:
-# $ python xvg_choose_column.py old_file.xvg 2
+# $ python xvg_choose_column.py -i old_file.xvg -c 2
 #
 
-import sys
+import argparse
 
-infile_name = sys.argv[1]
-column_num = sys.argv[2]
-outfile_name = '.'.join(infile_name.split('.')[:-1]) + '-column_' + column_num + '.' + infile_name.split('.')[-1]
+
+
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', required=True, nargs='+')
+parser.add_argument('-c', required=True)
+args = parser.parse_args()
+
+infile_name = args.f
+column_num = args.c
+
+
 
 
 # open files
-infile = open(infile_name,'r')
-outfile = open(outfile_name,'w')
+for f in infile_name:
+    infile = open(f,'r')
+    outfile_name = '.'.join(f.split('.')[:-1]) + '-column_' + column_num + '.' + f.split('.')[-1]
+    outfile = open(outfile_name,'w')
 
-for line in infile:
-	if not (line.startswith('#') or line.startswith('@')):
-		words=line.split()
-		outfile.write(words[0] + ' ' + words[int(column_num)] + '\n')
+    for line in infile:
+        if not (line.startswith('#') or line.startswith('@') or line.startswith('&')):
+            words=line.split()
+            print(words)
+            outfile.write(words[0] + ' ' + words[int(column_num)] + '\n')
 
-# close files
-infile.close() 
-outfile.close()
+    # close files
+    infile.close() 
+    outfile.close()
