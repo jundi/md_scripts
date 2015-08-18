c6a9c1bf23a99af90c936cae2b5d9fda3ff71d7a
h. mikkolainen <heikki.mikkolainen@gmail.com>
2015-08-18 14:44:11 +0300

    checkstates.sh script added

---
+++ bash/checkstates.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Looks for GROMACS state.cpt files and prints the time steps.
+#
+
+for state in $(find -name "state.cpt"); do
+  lframe=$(gmxcheck -f $state 2>&1 | grep "Last frame")
+  tstep=$(echo $lframe | awk '{print $5}')
+  folder=$(dirname $state)
+  echo "$folder   $tstep"
+done | sort
