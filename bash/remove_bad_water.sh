372dca37abfb5eff753f35f52fae401cec6b9486
h. mikkolainen <heikki.mikkolainen@gmail.com>
2015-08-18 14:47:46 +0300

    add remove_bad_water.sh script

---
+++ bash/remove_bad_water.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# 
+# Removes water molecules around selected atom group.
+#
+
+### defaults
+input="conf.gro"
+output="out.gro"
+structure="topol.tpr"
+index=""
+new_index="badwater.ndx"
+radius="0.5"
+group="Protein"
+
+### arguments
+while [[ $# -gt 0 ]]; do    
+  case "$1" in
+    -f)
+      input=$(readlink -f $2)
+      shift
+      ;;
+    -o)
+      output=$(readlink -f $2)
+      shift
+      ;;
+    -s)
+      structure=$(readlink -f $2)
+      shift
+      ;;
+    -n)
+      index="-n $(readlink -f $2)"
+      shift
+      ;;
+    -on)
+      new_index=$(readlink -f $2)
+      shift
+      ;;
+    -g)
+      group="$2"
+      shift
+      ;;
+  esac
+  shift       
+done
+
+
+### create selection
+selection="not (group Water and (same resid as (within $radius of group $group)))"
+g_select -on $new_index -f $input -s $structure $index -select "$selection"
+
+
+### write new file
+editconf -n $new_index -f $input -o $output
+
+
