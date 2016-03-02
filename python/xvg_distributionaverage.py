0ab7e8c3086e77f18309af0ab6388642b1eae677
h. mikkolainen <heikki.mikkolainen@gmail.com>
2016-03-02 20:47:28 +0200

    new script

---
+++ python/xvg_distributionaverage.py
@@ -0,0 +1,42 @@
+#!/bin/python
+#
+# Computes mean of distribution.
+#
+
+import argparse
+import sys
+import numpy as np
+
+
+### parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', nargs='*',help='xvg-file')
+parser.add_argument('-v',help='Be verbose.',action='store_true')
+
+args = parser.parse_args()
+file_names = args.f 
+
+
+averages=np.array([])
+for file_name in file_names:
+    ### read file
+    new_lines = []
+    data = []
+    with open(file_name,'r') as infile:
+        for line in infile:
+            if line.startswith('#') or line.startswith('@'):
+               new_lines.append(line)
+            else:
+               points = [float(x) for x in line.split()]
+               data.append(points)
+    
+    
+    ### compute
+    data = np.array(data) 
+    avg = sum(data[:,0]*data[:,1])/sum(data[:,1])
+    if args.v:
+        print(avg)
+    averages = np.append(averages, avg)
+
+### print
+print('mean: ' + str(averages.mean()) + ' +- ' +str(averages.std()))
