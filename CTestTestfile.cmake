6555d07195dabb82be50b3373e1766426f0c7f56
h. mikkolainen <heikki.mikkolainen@gmail.com>
2016-06-15 16:43:02 +0300

    add files

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mikkolah/koodi/ohjelmat/gromacs/gromacs-5.1.2/share/template
+# Build directory: /home/mikkolah/koodi/ohjelmat/gromacs/gromacs-5.1.2/share/template
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
