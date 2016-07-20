11147068c6ffdd50d2182614ef47c160d92b09ec
h. mikkolainen <heikki.mikkolainen@gmail.com>
2016-07-20 14:52:03 +0300

    help and comments

--- gromacs/distance/distance.cpp
+++ gromacs/distance/distance.cpp
@@ -1,45 +1,7 @@
 /*
- * This file is part of the GROMACS molecular simulation package.
- *
- * Copyright (c) 2010,2011,2012,2013,2014,2015, by the GROMACS development team, led by
- * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
- * and including many others, as listed in the AUTHORS file in the
- * top-level source directory and at http://www.gromacs.org.
- *
- * GROMACS is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- *
- * GROMACS is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with GROMACS; if not, see
- * http://www.gnu.org/licenses, or write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
- *
- * If you want to redistribute modifications to GROMACS, please
- * consider that scientific software is very special. Version
- * control is crucial - bugs must be traceable. We will be happy to
- * consider code for inclusion in the official distribution, but
- * derived work must not be called official GROMACS. Details are found
- * in the README & COPYING files - if they are missing, get the
- * official version at http://www.gromacs.org.
- *
- * To help us fund GROMACS development, we humbly ask that you cite
- * the research papers on the package. Check out http://www.gromacs.org.
+ * This file is modified version of a file distributed as part of the
+ * GROMACS molecular simulation package.
  */
-/*! \internal \file
- * \brief
- * Implements gmx::analysismodules::Distance.
- *
- * \author Teemu Murtola <teemu.murtola@gmail.com>
- * \ingroup module_trajectoryanalysis
- */
-//#include "gmxpre.h"
 
 #include "distance.h"
 
@@ -113,25 +75,8 @@ void
 Distance::initOptions(Options *options, TrajectoryAnalysisSettings * /*settings*/)
 {
     static const char *const desc[] = {
-        "[THISMODULE] calculates distances between pairs of positions",
-        "as a function of time. Each selection specifies an independent set",
-        "of distances to calculate. Each selection should consist of pairs",
-        "of positions, and the distances are computed between positions 1-2,",
-        "3-4, etc.[PAR]",
-        "[TT]-oav[tt] writes the average distance as a function of time for",
-        "each selection.",
-        "[TT]-oall[tt] writes all the individual distances.",
-        "[TT]-oxyz[tt] does the same, but the x, y, and z components of the",
-        "distance are written instead of the norm.",
-        "[TT]-oh[tt] writes a histogram of the distances for each selection.",
-        "The location of the histogram is set with [TT]-len[tt] and",
-        "[TT]-tol[tt]. Bin width is set with [TT]-binw[tt].",
-        "[TT]-oallstat[tt] writes out the average and standard deviation for",
-        "each individual distance, calculated over the frames.[PAR]",
-        "Note that [THISMODULE] calculates distances between fixed pairs",
-        "(1-2, 3-4, etc.) within a single selection.  To calculate distances",
-        "between two selections, including minimum, maximum, and pairwise",
-        "distances, use [gmx-pairdist].asdfasdfadfs"
+	"[THISMODULE] calculates distances between a position and a set of",
+	"positions as a function of time.",
     };
 
     options->setDescription(desc);
@@ -146,7 +91,7 @@ Distance::initOptions(Options *options, TrajectoryAnalysisSettings * /*settings*
                            .store(&fnAbsZ_).defaultBasename("absdistz")
                            .description("Absolute distance z-component as function of time"));
     options->addOption(SelectionOption("select").store(&sel_).required()
-                           .description("Positions to calculate distances for"));
+                           .description("Group to calculate distances for"));
     options->addOption(SelectionOption("ref").store(&ref_).required()
                            .description("Reference position"));
 }
