28fd16012c3ef9729fb9e16916733ce5e1229497
h. mikkolainen <heikki.mikkolainen@gmail.com>
2016-08-10 12:25:25 +0300

    choose columns

--- python/xvg_cumsum.py
+++ python/xvg_cumsum.py
@@ -1,7 +1,8 @@
 #!/bin/python
-# 
-# Cumulative sum of columns.
-#
+""" 
+Cumulative sum of columns. Columns to be summed can be chosen with -c.
+Default is all except first column.
+"""
 
 import argparse
 import numpy
@@ -9,18 +10,25 @@ import xvgio
 
 
 ### parse arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', nargs='+')
-parser.add_argument('-o')
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('-f',help="Input file")
+parser.add_argument('-o',help="Output file")
+parser.add_argument('-c',help="Columns",nargs='+',type=int)
 
 args = parser.parse_args()
-infile_names = args.f 
+infile_name = args.f 
 outfile_name = args.o
+columns = args.c
 
 # read file
 data, comment = xvgio.read(infile_name)
 
-data=numpy.cumsum(data,axis=0)
+if not (columns):
+    columns=range(1, data.shape[1])
+
+result=data # init array
+for c in columns:
+    result[:,c]=numpy.cumsum(data[:,c],axis=0)
 
 ### write
-xvgio.write(outfile_name, data, comment)
+xvgio.write(outfile_name, result, comment)
