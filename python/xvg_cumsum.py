55e8bfbc79014a8237d12273305d8c5eab40d8c6
h. mikkolainen <heikki.mikkolainen@gmail.com>
2016-08-22 19:18:59 +0300

    SSE

--- python/xvg_cumsum.py
+++ python/xvg_cumsum.py
@@ -1,7 +1,8 @@
 #!/bin/python
 """ 
-Cumulative sum of columns. Columns to be summed can be chosen with -c.
-Default is all except first column.
+Cumulative sum of columns. Columns to be summed can be chosen with -c.  Default
+is all except first column.  For columns chosen with -e the sum of squares
+(SSE) is calculated.
 """
 
 import argparse
@@ -14,11 +15,13 @@ parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('-f',help="Input file")
 parser.add_argument('-o',help="Output file")
 parser.add_argument('-c',help="Columns",nargs='+',type=int)
+parser.add_argument('-e',help="Error columns",nargs='+',type=int)
 
 args = parser.parse_args()
 infile_name = args.f 
 outfile_name = args.o
 columns = args.c
+error_columns = args.e
 
 # read file
 data, comment = xvgio.read(infile_name)
@@ -29,6 +32,11 @@ if not (columns):
 result=data # init array
 for c in columns:
     result[:,c]=numpy.cumsum(data[:,c],axis=0)
+
+if args.e:
+    for e in error_columns:
+        result[:,e]=numpy.cumsum(data[:,e]*data[:,e],axis=0)
+        result[:,e]=numpy.sqrt(result[:,e])
 
 ### write
 xvgio.write(outfile_name, result, comment)
