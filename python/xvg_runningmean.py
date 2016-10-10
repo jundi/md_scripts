b2fd59d07ea363c773fa6f39dc8810fd1d136624
h. mikkolainen <heikki.mikkolainen@gmail.com>
2016-10-10 14:01:26 +0300

    simple moving mean

--- python/xvg_runningmean.py
+++ python/xvg_runningmean.py
@@ -1,10 +1,11 @@
 #!/bin/python
 #
-# Computes running mean
+# Computes simple running mean.
 #
 
 import argparse
 import sys
+import math
 import os
 import xvgio
 import numpy as np
@@ -14,13 +15,13 @@ import numpy as np
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', required=True)
 parser.add_argument('-o')
-parser.add_argument('-n', type=int, default=10)
+parser.add_argument('-n', type=int, default=9)
 
 args = parser.parse_args()
 
 width = int(args.n)
-if width < 2:
-    sys.exit('n < 2')
+if width % 2 != 1:
+    sys.exit('n has to be odd integer')
 
 infile_name = args.f 
 
@@ -35,14 +36,15 @@ else:
 ### read file
 data, comment = xvgio.read(infile_name)
 
-### compute means
-rows = data.shape[0]
-columns = data.shape[1]
-newdata = np.zeros([int(rows/width-1), columns])
-for r in range(int(rows/width-1)):
-    for c in range(columns):
-        newdata[r,c] = np.mean(data[r*width:r*width+width-1,c])
 
+### compute means
+columns = data.shape[1]
+newdata=data[math.floor(width/2):-math.floor(width/2),:]
+for c in range(1,columns):
+    column = np.cumsum(data[:,c])
+    column[width:] = column[width:] - column[:-width]
+    column = column[width-1:] / width
+    newdata[:,c]=column
 
 
 ### write
